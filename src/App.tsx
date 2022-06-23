05b0067f22f668cc81706ad5544c0cff589f5d69
Pablo Alan <pabloxt14@gmail.com>
2022-06-23 18:41:29 -0300

    feat: sidebar implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react"
-import { client } from "./lib/apollo";
-
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 export function App() {
-  // useEffect(() => {
-  //   // REQUISIÇÃO COM APOLLO CLIENTE (MANEIRA NÃO COMUM)
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response => {
-  //     console.log(response.data);
-  //   })
-  // }, []);
-
-  
-  // REQUISIÇÃO COM APOLLO CLIENTE (MANEIRA MAIS USUAL)
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-  console.log(data);
-
+ 
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
